6514b0303f4247caf7ede78531a728d1ff4342ca
Tom Esterez <testerez@gmail.com>
2018-02-23 02:39:31 -0500

    smaller, better

--- src/index.ts
+++ src/index.ts
@@ -2,131 +2,94 @@
 // https://closure-compiler.appspot.com/home
 // http://siorki.github.io/regPack.html
 
-interface Point {
-  x: number;
-  y: number;
-}
-
-interface Direction {
-  x: number,
-  y: number,
-}
-
-let pointsEq = (a: Point) => (b: Point) => a.x == b.x && a.y == b.y; 
-
-let drawPoint = (ctx: CanvasRenderingContext2D, p: Point, color: string) => {
-  ctx.fillStyle = color;
-  ctx.fillRect(p.x, p.y, 1, 1);
-}
-
-// Game size
-const scale = 15;
-const w = 40;
-const h = 30;
-
-// Canvas
+type Point = [number, number];
+type Direction = [number, number];
 declare const c: CanvasRenderingContext2D;
-c.scale(scale, scale);
 
-// Game state
-let food: Point;
-let snakePoints = [{ x: w / 2, y: h / 2 }];
-let eatenCount = 0;
-let step = 100;
-let running = true;
-let snakeDir: Direction | null;
-let snakeNextDir: Direction | null;
+let
+  scale = 15,
+  w = 40,
+  h = 30, 
+  pointEq = ([ax, ay]: Point) => ([bx, by]: Point) => ax == bx && ay == by,
+  drawPoint = (ctx: CanvasRenderingContext2D, [x, y]: Point, color: string) => {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, 1, 1);
+  },
 
-function createFood(){
-  // TODO: optimize
-  let f: Point;
-  do {
-    f = {
-      x: Math.floor(Math.random() * w),
-      y: Math.floor(Math.random() * h),
-    };
-    food = f;
-  } while(snakePoints.some(pointsEq(f)))
-}
-createFood();
+  // Game state
+  food: Point,
+  snakePoints: Point[] = [[w / 2, h / 2 ]],
+  eatenCount = 0,
+  snakeDirQueue = [] as Direction[], // queue directions
+  snakeDir: Direction | undefined,
 
-let accumulator = 0;
-function loop(time: number) {
-  accumulator = accumulator || time;
-  while(accumulator <= time){
-    // update game state
-    if (running) {
-      const headPos = snakePoints[0];
+  createFood = () => {
+    food = [w, h].map(size => Math.floor(Math.random() * size)) as Point;
+    // if food is under the snake, re-create it
+    snakePoints.some(pointEq(food)) && createFood();
+  },
+  loop = () => {
+    snakeDir = snakeDirQueue.shift() || snakeDir;
+    if (snakeDir) { // update game state
+      const
+        headPos = snakePoints[snakePoints.length - 1]
+          .map((value, axis) => value + snakeDir![axis]) as Point,
+        [x, y] = headPos;
+
+      // check if lost
+      if (
+        snakePoints.some(pointEq(headPos)) ||
+        x < 0 ||
+        y < 0 ||
+        x >= w ||
+        y >= h
+      ) {
+        c.font = '3px impact';
+        c.fillStyle = '#f00';
+        c.textAlign = 'center';
+        c.fillText('GAME OVER', 20, 15);
+        return; // Exit game loop
+      }
+
+      snakePoints.push(headPos);
 
       // food
-      if (pointsEq(headPos)(food)) {
+      if (pointEq(headPos)(food)) {
         createFood();
         eatenCount++;
-        step = step * 0.999; // Speed-up game
       }
 
-      // move snake
-      const dir = snakeNextDir || snakeDir;
-      if (dir) {
-        snakeNextDir = null;
-
-        const newHeadPos = {
-          x: headPos.x + dir.x,
-          y: headPos.y + dir.y,
-        }
-
-        // check if lost
-        if(
-          snakePoints.some(pointsEq(newHeadPos)) ||
-          newHeadPos.x < 0 ||
-          newHeadPos.x >= w ||
-          newHeadPos.y < 0 ||
-          newHeadPos.y >= h
-        ) {
-          c.font = "5px Impact";
-          c.fillStyle = "#f00";
-          c.textAlign= "center"; 
-          c.fillText(`Your score: ${eatenCount}`, w / 2, h / 2);
-          return; // Exit game loop
-        }
-        snakePoints.unshift(newHeadPos);
-        snakePoints = snakePoints.slice(0, (eatenCount * 3) + 10);
-      }
+      snakePoints = snakePoints.slice(-eatenCount * 3 - 9);
     }
 
-    accumulator += step;
-  }
+    // draw
+    c.fillStyle = '#000';
+    c.fillRect(0, 0, w, h);
+    drawPoint(c, food, '#09f');
+    snakePoints.forEach(p => drawPoint(c, p, `#fff`));
 
-  // draw
-  c.fillStyle = "#000";
-  c.fillRect(0, 0, w, h);
-  let color = 255;
-  snakePoints.forEach(p => {
-    const c2 = Math.round(color);
-    drawPoint(c, p, `rgb(${c2},${c2},${c2})`);
-    color *= 0.997;
-  })
-  drawPoint(c, food, '#09f');
+    c.font = '1px Verdana';
+		c.fillStyle = '#fff';
+		c.fillText(eatenCount as any, 1, 1.5);
 
 
-  requestAnimationFrame(loop);
-}
-requestAnimationFrame(loop);
+    setTimeout(loop, 100 * (0.999 ** (eatenCount + 1)));
+  };
 
-document.addEventListener('keydown', e => {
-  const k = e.keyCode;
-  const x =
-    k == 37 ? -1 :
-    k == 39 ? 1 :
-    0;
-  const y =
-    k == 38 ? -1 :
-    k == 40 ? 1 :
-    0;
+// run game
+c.scale(scale, scale);
+c.shadowColor = '#000';
+c.shadowBlur = 15;
+createFood();
+loop();
+
+// user inputs
+onkeydown = ({ keyCode: k }) => {
+  const
+    x = k == 37 ? -1 : k == 39 ? 1 : 0,
+    y = k == 38 ? -1 : k == 40 ? 1 : 0,
+    [nextX, nextY] = snakeDirQueue[0] || snakeDir || [] as any;
     
-  // Can't strop and Can't move on itself
-  if ((x || y) && !(snakeDir && (snakeDir.x && x || snakeDir.y && y))) {
-    snakeDir = { x, y };
-    snakeNextDir = snakeNextDir || snakeDir;
-  }
-});
+  // Can't stop and Can't move on itself
+  (x && !x != !nextX || y && !y != !nextY) && snakeDirQueue.push([x, y]);
+};
